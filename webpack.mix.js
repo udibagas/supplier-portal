e5ebadeabde31cf3b008895da56d3fd035e4b264
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-08-14 09:20:20 +0700

    update assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,8 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/create-vendor.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+      processCssUrls: false,
+      processImageUrls: false
+    });
