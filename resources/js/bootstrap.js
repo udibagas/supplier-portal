038e8d02ce75d67130ff58998e27cf71569e11ab
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2020-12-21 11:39:12 +0700

    csrf token

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,4 +3,13 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.baseURL = BASE_URL;
 window.axios.defaults.headers.common['Authorization'] = 'bearer ' + window.localStorage.getItem('token');
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
 window.moment = require('moment');
